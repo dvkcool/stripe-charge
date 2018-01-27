6c424e06e1874ae8a9d9ff13deb0a1435cb05747
dvkcool <divyanshukumarg@gmail.com>
2018-01-27 16:01:25 +0530

    Trying to run basic page, unknown error

--- src/scenes/Allcharges.js
+++ src/scenes/Allcharges.js
@@ -1,75 +1,20 @@
 import React, { Component } from 'react';
-import {Container, Left, Right, Card, CardItem  } from 'native-base';
-import { View,  ListView, Text} from 'react-native';
+import { View,  Container, ListView,  ActivityIndicator, Text} from 'react-native';
 export default class Allcharges extends Component {
   static title = 'Last 10 charges/transactions';
- state = {
-    isLoading: true,
-  };
-
-  componentDidMount() {
-    return fetch('https://api.ascendancy10.hasura-app.io/charges/10', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        key: "sk_test_BQokikJOvBiI2HlWgH4olfQ2"
-      })
-    })
-    .then((response) => {
-    let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-    this.setState({
-      isLoading: false,
-      dataSource: ds.cloneWithRows(response.data),
-    }, function() {});
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-  }
   render() {
-    if (this.state.isLoading) {
-      return (
-        <View style={{flex: 1, paddingTop: 20}}>
-        <ActivityIndicator />
-        </View>
-      );
-    }
-    else{
+
       return (
         <Container>
-          <ListView
-          dataSource={this.state.dataSource}
-          style={{paddingRight: 10,}}
-          renderRow={(rowData) =>
-              <Card style={{flex: 1}}>
-              <CardItem>
-                <Left>
-                  <Text> Amount:  {rowdata.amount} </Text>
-                </Left>
-                <Body/>
-                <Right/>
-              </CardItem>
-              <CardItem>
-                <Left/>
-                <Body/>
-                <Right>
-                  <Text style={{color: '#99FF00'}}> Succes: <Text style={{color: '#FF4600'}}> {rowddata.paid} </Text></Text>
-                </Right>
-              </CardItem>
-              <CardItem>
-                <Left>
-                  <Text> Charge id:   <Text style={{color: '#99FF00'}}> {rowdata.id} </Text></Text>
-                </Left>
-              </CardItem>
-              </Card>
-          }
-          />
+                  <View>
+                  <Text> Amount: </Text>
+                  <Text style={{color: '#99FF00'}}> Succes: </Text><Text style={{color: '#FF4600'}}> Hi2 </Text>
+                  <Text> Charge id:  </Text> <Text style={{color: '#99FF00'}}>hi  </Text>
+                  </View>
         </Container>);
 
     }
-  }
+
 
 
 }
